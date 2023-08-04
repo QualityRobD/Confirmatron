d7942b5343111e26c449e27a360bb4eb25665727
rduncan <17755173+QualityRobD@users.noreply.github.com>
2023-08-04 15:58:05 -0400

    Added missing auth0 setup lines in api_one config

--- config/apis/api_one.py
+++ config/apis/api_one.py
@@ -6,24 +6,32 @@ def setup():
     api_one = Api('apiOne')
 
     # Setup api_one details
-    api_one.app_id = 'app_id_for_apiOne'
-    api_one.base_url.test = 'test_url_for_apiOne'
-    api_one.base_url.beta = 'beta_url_for_apiOne'
-    api_one.base_url.prod = 'prod_url_for_apiOne'
-    api_one.teams_channel_link = 'teams_channel_link_for_apiOne'
+    api_one.app_id = ''
+    api_one.base_url.test = ''
+    api_one.base_url.beta = ''
+    api_one.base_url.prod = ''
+    api_one.teams_channel_link = ''
 
     # Setup auth_zero details for api_one
-    api_one.auth_zero.test.audience = 'test_audience_for_apiOne'
-    api_one.auth_zero.test.grant_type = 'test_grant_type_for_apiOne'
-    api_one.auth_zero.beta.audience = 'beta_audience_for_apiOne'
-    api_one.auth_zero.beta.grant_type = 'beta_grant_type_for_apiOne'
-    api_one.auth_zero.prod.audience = 'prod_audience_for_apiOne'
-    api_one.auth_zero.prod.grant_type = 'prod_grant_type_for_apiOne'
+    api_one.auth_zero.test.audience = ''
+    api_one.auth_zero.test.grant_type = ''
+    api_one.auth_zero.test.client_id = ''
+    api_one.auth_zero.test.client_secret = ''
+
+    api_one.auth_zero.beta.audience = ''
+    api_one.auth_zero.beta.grant_type = ''
+    api_one.auth_zero.beta.client_id = ''
+    api_one.auth_zero.beta.client_secret = ''
+
+    api_one.auth_zero.prod.audience = ''
+    api_one.auth_zero.prod.grant_type = ''
+    api_one.auth_zero.prod.client_id = ''
+    api_one.auth_zero.prod.client_secret = ''
 
     # Setup controllers_under_test details for api_one
-    api_one.controllers_under_test.test = ['test_controller_1', 'test_controller_2']
-    api_one.controllers_under_test.beta = ['beta_controller_1', 'beta_controller_2']
-    api_one.controllers_under_test.prod = ['prod_controller_1', 'prod_controller_2']
+    api_one.controllers_under_test.test = ['', '']
+    api_one.controllers_under_test.beta = ['', '']
+    api_one.controllers_under_test.prod = ['', '']
 
     # Add api_one to SecretsConfig
     config = Config()
